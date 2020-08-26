791f4585a317ed7df7c40a0ceb736711873a9a7e
Nicholas Otieno <niklasoti@gmail.com>
2020-08-26 13:08:51 +0200

    Update app.component.ts
    Get images from unsplash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,21 +9,23 @@ export class AppComponent  {
   posts = [
     {
       title: 'Neat Tree',
-      imageUrl: 'https://cdn.jsdelivr.net/gh/Niklus/ng-cards@master/src/assets/tree.jpeg',
+      imageUrl: 'https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg',
       username: 'nature',
       content: 'Check out this awesome tree I saw today.'
     },
     {
       title: 'Snowy Mountain',
-      imageUrl: 'https://cdn.jsdelivr.net/gh/Niklus/ng-cards@master/src/assets/mountain.jpeg',
+      imageUrl: 'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b',
       username: 'mountainlover',
       content: 'Here is a picture of a snowy mountain.'
     },
     {
       title: 'Mountain Biking',
-      imageUrl: 'https://cdn.jsdelivr.net/gh/Niklus/ng-cards@master/src/assets/biking.jpeg',
+      imageUrl: 'https://images.unsplash.com/photo-1515412512744-6e4adc8b5e55',
       username: 'Biking1222',
       content: 'I did some biking today!'
     }
   ]
 }
+
+// images from unsplash
